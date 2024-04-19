0783daa5fd415017ca13a59bda5c1e3816417c6e
matiteijiz <teijiz.matias@gmail.com>
2024-04-19 17:29:04 -0300

    Init commit

---
+++ src/main/kotlin/com/clone/trello/TrelloApplication.kt
@@ -0,0 +1,11 @@
+package com.clone.trello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrelloApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrelloApplication>(*args)
+}
